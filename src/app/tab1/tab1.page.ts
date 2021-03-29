9b824c6348ab305c7571cba63d0d738d52d2254b
HendrikChris <79140979+HendrikChris@users.noreply.github.com>
2021-03-29 23:55:44 +0700

    foto

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FotoService } from '../services/foto.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor(public fotoService : FotoService) {}
+
+  tambahFoto(){
+    this.fotoService.tambahFoto();
+  }
+}
